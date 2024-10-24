82230fd690c4dff0b0f7c31fce2e656572feda8b
Dellina-Medhanie <dellina.medhanie@simmons.edu>
2024-10-24 11:09:38 -0400

    Create DataTypes.js

---
+++ DataTypes.js
@@ -0,0 +1,29 @@
+// integer
+let age = 25;
+
+// string
+let name = "Dellina"; 
+
+// floating point number
+let floatingPointNumber = 10.99;
+
+// boolean value
+let working = true; 
+
+// list
+let university = ["Simmons", "CS330", "Nanette"]; 
+
+// dictionary
+let person = { 
+    name: "Manna", 
+    age: 57, 
+    working: true 
+}; 
+
+// This will produce the output in the terminal
+console.log("Integer:", age);
+console.log("String:", name);
+console.log("Floating-point number:", floatingPointNumber);
+console.log("Boolean:", working);
+console.log("Array:", university);
+console.log("Dictionary/Hash/Map:", person);
